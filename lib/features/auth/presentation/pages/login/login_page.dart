716a8c9611ee908d7d2468176ed98aae4c083ff3
rizkirchm97 <rizkirchm6@gmail.com>
2023-07-01 00:16:12 +0700

    Change Container to Padding to wrapping Otp Form

--- lib/features/auth/presentation/pages/login/login_page.dart
+++ lib/features/auth/presentation/pages/login/login_page.dart
@@ -22,10 +22,9 @@ class LoginPage extends StatelessWidget {
   _buildBody() {
     return BlocBuilder<LoginCubit, LoginState>(
       builder: (context, state) {
-        return Container(
-          margin: const EdgeInsets.only(top: 100, left: 20, right: 20),
-          height: double.infinity,
-          child: const LoginForm(),
+        return const Padding(
+          padding: EdgeInsets.only(top: 100, left: 20, right: 20),
+          child: LoginForm(),
         );
       },
     );
