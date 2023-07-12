7d288cc9b4138854f9232f46afce6790899bb081
rizkirchm97 <rizkirchm6@gmail.com>
2023-07-12 12:06:49 +0700

    Add custom Ticker Functionality and registered it in app_module

--- lib/app_module.dart
+++ lib/app_module.dart
@@ -1,17 +1,22 @@
 
 import 'package:get_it/get_it.dart';
+import 'package:savfi/core/helper/ticker.dart';
 import 'package:savfi/features/auth/presentation/bloc/login_bloc/login_cubit.dart';
 import 'package:savfi/features/auth/presentation/bloc/otp_bloc/otp_cubit.dart';
 import 'package:savfi/features/auth/presentation/bloc/register_bloc/register_cubit.dart';
+import 'package:savfi/features/splashscreen/presentation/bloc/splash_screen_cubit.dart';
 
 final sl = GetIt.instance;
 
 Future<void> init() async {
 
+  // Helper
+  sl.registerSingleton<Ticker>(Ticker());
 
   // Bloc/Cubit
   sl.registerFactory(() => LoginCubit());
   sl.registerFactory(() => OtpCubit());
   sl.registerFactory(() => RegisterCubit());
+  sl.registerFactory(() => SplashScreenCubit(ticker: sl()));
 
 }
