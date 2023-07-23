0f580e6295700f6eefe7c139a04c038960a26069
rizkirchm97 <rizkirchm6@gmail.com>
2023-07-24 02:17:16 +0700

    add dummy Note and Track Module

---
+++ lib/features/note_and_track/presentation/pages/note_and_track_page.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+
+@RoutePage()
+class NoteAndTrackPage extends StatelessWidget {
+  const NoteAndTrackPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Note and Track Page'),
+    );
+  }
+}
